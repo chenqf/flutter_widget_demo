b61a5df9d80efd783cbd83943d0fa09b52df0429
chenqf <qifeng.chen1006@foxmail.com>
2019-01-03 14:29:09 +0800

    组件 Container 简单相关内容

--- lib/demo/container.dart
+++ lib/demo/container.dart
@@ -10,17 +10,27 @@ class MyContainer extends StatelessWidget {
         ),
         body: Center(
           child: Container(
+            margin: EdgeInsets.only(left: 100.0),
             padding: EdgeInsets.all(20.0),
-            alignment: Alignment.center, // 控制字内容的对其位置
+            alignment: Alignment.bottomLeft, // 控制字内容的对其位置
             width: 200.0,
             height: 200.0,
 //            color: Colors.lightBlueAccent,
             decoration: BoxDecoration(
-              gradient: RadialGradient(
-                colors: [Colors.red,Colors.orange],
-                center: Alignment.topLeft,
-                radius: 1
+              //径向渐变
+//              gradient: RadialGradient(
+//                colors: [Colors.red,Colors.deepPurple],
+//                center: Alignment.topLeft,
+//                radius: 1
+//              ),
+              gradient:LinearGradient(
+                  colors: [Colors.purple,Colors.red,Colors.orange],
               ),
+              border: Border.all(
+                width: 2.0,
+                color: Colors.black
+              ),
+              borderRadius: BorderRadius.all(Radius.circular(10.0)),  
               boxShadow: [
                 BoxShadow(
                   color: Colors.black12,
