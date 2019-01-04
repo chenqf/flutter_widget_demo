3cd5f0fade9ff57f89b2d583df1ba4c366b35dd2
chenqf <qifeng.chen1006@foxmail.com>
2019-01-04 14:09:05 +0800

    组件 GridView 简单相关内容

--- lib/demo/gridView/index.dart
+++ lib/demo/gridView/index.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './1.dart';
+import './2.dart';
 
 
 class MyGridView extends StatelessWidget {
@@ -25,6 +26,22 @@ class MyGridView extends StatelessWidget {
                   Navigator.push( context,
                       new MaterialPageRoute(builder: (context) {
                         return new MyGridView1();
+                      })
+                  );
+                },
+              ),
+              FlatButton(
+                child: Text("GridView - 2"),
+                color: Colors.blue,
+                highlightColor: Colors.blue[700],
+                colorBrightness: Brightness.dark,
+                splashColor: Colors.grey,
+                shape:RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+                onPressed: () {
+                  //导航到新路由
+                  Navigator.push( context,
+                      new MaterialPageRoute(builder: (context) {
+                        return new MyGridView2();
                       })
                   );
                 },
